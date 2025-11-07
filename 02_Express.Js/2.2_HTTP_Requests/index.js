17cf111e21f6d201c8a9328afd2e2dbb37e42c1e
ani29dy <aniketanni2916@gmail.com>
2025-11-07 10:46:05 +0530

    Updated get request

--- 02_Express.Js/2.2_HTTP_Requests/index.js
+++ 02_Express.Js/2.2_HTTP_Requests/index.js
@@ -4,8 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  // res.send("Hello World!!!");
-  res.send("How are You?");
+  res.send("Hello World!!!");
 });
 
 app.get("/about", (req, res) => {
