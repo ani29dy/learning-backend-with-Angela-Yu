858331ac3d2116fff7b27b198b39187a6f38ae60
ani29dy <aniketanni2916@gmail.com>
2025-11-12 15:21:20 +0530

    Handling Requests using Custom Middleware

--- 02_Express.Js/2.4_Middleware/index3.js
+++ 02_Express.Js/2.4_Middleware/index3.js
@@ -3,6 +3,22 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// app.use((req, res, next) => {
+//   console.log("Request Method: ", req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Request URL: ", req.url);
+//   next();
+// });
+
+const logger = (req, res, next) => {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  next();
+};
+
 app.use(logger);
 
 app.get("/", (req, res) => {
